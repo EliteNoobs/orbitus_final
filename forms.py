310b70eac4134c2d43f48429c35f30bfd40ec1b7
Kumar Swayam <kumaryash.407@gmail.com>
2018-10-12 21:41:41 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -9,13 +9,13 @@ class Register(FlaskForm):
 
 class LogIn(FlaskForm):
 	Username = StringField('Username', validators=[DataRequired(), Length(min=6)])
-	Password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=63)])
+	Password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=64)])
 	RememberMe = BooleanField('Remember Me')
 	login = SubmitField('Log In')
 
 class Username(FlaskForm):
 	Username = StringField('Username', validators=[DataRequired(), Length(min=6)])
-	Password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=63)])
+	Password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=64)])
 	ConfirmPassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('Password')])
 	SignUp = SubmitField('SignUp')
 	
