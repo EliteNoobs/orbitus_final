f7d677a44b2a5c89731df8a910bb209a15c2364a
Cyber-Boy <34743284+Cyber-Boy@users.noreply.github.com>
2018-10-27 05:15:30 -0700

    Add files via upload

--- orbitus/__init__.py
+++ orbitus/__init__.py
@@ -7,6 +7,7 @@ Orbitus = Flask(__name__)
 Orbitus.config['SECRET_KEY'] = '137173d918599668dd83e68db2bcad2e'
 #Added a basic Database
 Orbitus.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+#Orbitus.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@127.0.0.1:8001/flask'
 db = SQLAlchemy(Orbitus)
 crypter = Bcrypt(Orbitus) # Do not mess with this... Especially Kushagra
 
--- orbitus/models.py
+++ orbitus/models.py
@@ -2,17 +2,23 @@ from orbitus import db
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	fullname = db.Column(db.String, nullable=False)
-	email = db.Column(db.String,unique=True, nullable=False)
-	Username = db.Column(db.String,unique=True, nullable=False)
-	password = db.Column(db.String(64), nullable=False)
+	fullname = db.Column(db.String(40))#, nullable=False)
+	email = db.Column(db.String(40))#,unique=True, nullable=False)
+	Username = db.Column(db.String(40))#,unique=True, nullable=False)
+	password = db.Column(db.String(64))#, nullable=False)
+	def __repr__(self):
+		return f"('{self.fullname}', '{self.email}', '{self.Username}')"
+
 
 class Group(db.Model):
 	groupid = db.Column(db.Integer, primary_key=True)
 	groupname = db.Column(db.String(32), nullable=False)
 	
-	def __repr__(self):
-		return f"('{self.fullname}', '{self.email}', '{self.Username}')"
 	
-
-#It is not linked with the server yet.
+class Main(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	FullName = db.Column(db.String(40))#, nullable=False)
+	EMAIL = db.Column(db.String(40))#,unique=True, nullable=False)
+	Username = db.Column(db.String(40))#,unique=True, nullable=False)
+	Age = db.Column(db.Integer)#, nullable=False)
+	Password = db.Column(db.String(64))#, nullable=False)
