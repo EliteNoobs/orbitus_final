b2a90585516dc5629daab340746a1f712d692bb6
Kushagra Srivastava <kushagra1403@gmail.com>
2018-11-06 16:52:56 +0530

    models.py

--- orbitus/models.py
+++ orbitus/models.py
@@ -1,6 +1,6 @@
 from orbitus import db, login_manager
 from flask_login import UserMixin
-
+from sqlalchemy import func
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -8,10 +8,16 @@ def load_user(user_id):
 
 
 class Group(db.Model):
-	groupid = db.Column(db.Integer, primary_key=True)
+	id = db.Column(db.Integer, primary_key=True)
 	groupname = db.Column(db.String(32), nullable=False)
-	
-	
+	description = db.Column(db.String(120), nullable=False)
+	default_picture = db.Column(db.String(20), default='default.jpg')
+	member_id = db.Column(db.Integer, db.ForeignKey('main.id'), nullable=False)
+
+	def __repr__(self):
+		return f"('{self.id}', '{self.groupname}', '{self.description}'')"
+
+
 class Main(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key=True)
 	FullName = db.Column(db.String(40), nullable=False)
@@ -19,5 +25,7 @@ class Main(db.Model, UserMixin):
 	Username = db.Column(db.String(40),unique=True, nullable=False)
 	Age = db.Column(db.Integer, nullable=False)
 	Password = db.Column(db.String(64), nullable=False)
+	group = db.relationship('Group', backref='member', lazy=True)
 	def __repr__(self):
 		return f"('{self.FullName}', '{self.EMAIL}', '{self.Username}')"
+
